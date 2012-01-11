bbeef18f9c7e524c9ed02bb3f5ad7a073e2b3bd8
Evgeny V. Generalov <e.generalov@gmail.com>
2012-01-11 15:10:51 +0400

    use my pyflakes-vim fork

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,7 +39,10 @@ git clone git://github.com/int3/vim-taglist-plus.git bundle/taglist-plus; which 
 git clone git://github.com/bogado/file-line.git bundle/file-line
 
 # Python
-git clone git://github.com/kevinw/pyflakes-vim.git bundle/pyflakes-vim
+git clone git://github.com/generalov/pyflakes-vim.git bundle/pyflakes-vim
+sed -i 's#git@github.com:kevinw/pyflakes.git#git://github.com/kevinw/pyflakes.git#g' bundle/pyflakes-vim/.git/config
+sed -i 's#git@github.com:kevinw/pyflakes.git#git://github.com/kevinw/pyflakes.git#g' bundle/pyflakes-vim/.gitmodules
+
 git clone git://github.com/fs111/pydoc.vim.git bundle/pydoc
 git clone git://github.com/vim-scripts/pep8.git bundle/pep8
 #git clone git://github.com/generalov/pytest.vim.git bundle/managepy.test
@@ -62,9 +65,6 @@ git clone git://github.com/davidoc/taskpaper.vim.git bundle/taskpaper
 git clone git://github.com/tpope/vim-markdown.git bundle/vim-markdown
 
 
-sed -i 's#git@github.com:kevinw/pyflakes.git#git://github.com/kevinw/pyflakes.git#g' bundle/pyflakes-vim/.git/config
-sed -i 's#git@github.com:kevinw/pyflakes.git#git://github.com/kevinw/pyflakes.git#g' bundle/pyflakes-vim/.gitmodules
-
 ls bundle | while read line; do
 (
     cd bundle/$line
@@ -73,37 +73,3 @@ ls bundle | while read line; do
 )
 done
 
-patch -d bundle/pyflakes-vim -p1 <<EOD
---- a/ftplugin/python/pyflakes.vim
-+++ b/ftplugin/python/pyflakes.vim
-@@ -47,7 +47,10 @@ scriptdir = os.path.join(os.path.dirname(vim.eval('expand("<sfile>")')), 'pyflak
- if scriptdir not in sys.path:
-     sys.path.insert(0, scriptdir)
- 
--import ast
-+try:
-+    import ast
-+except ImportError:
-+    import _ast as ast
- from pyflakes import checker, messages
- from operator import attrgetter
- import re
-@@ -68,7 +71,7 @@ class blackhole(object):
- 
- def check(buffer):
-     filename = buffer.name
--    contents = buffer[:]
-+    contents = list(buffer)[:]
- 
-     # shebang usually found at the top of the file, followed by source code encoding marker.
-     # assume everything else that follows is encoded in the encoding.
-@@ -96,7 +99,7 @@ def check(buffer):
-         # TODO: use warnings filters instead of ignoring stderr
-         old_stderr, sys.stderr = sys.stderr, blackhole()
-         try:
--            tree = ast.parse(contents, filename or '<unknown>')
-+            tree = compile(contents, filename or '<unknown>', 'exec', ast.PyCF_ONLY_AST)
-         finally:
-             sys.stderr = old_stderr
-     except:
-EOD
